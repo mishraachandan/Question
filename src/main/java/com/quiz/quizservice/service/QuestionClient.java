1e0be47b6e25fb6c865b95b45d040e9cc0f003a8
Chandan Mishra <chandan345mishra@gmail.com>
2024-01-09 23:47:12 +0530

    Load Balancing changes

--- src/main/java/com/quiz/quizservice/service/QuestionClient.java
+++ src/main/java/com/quiz/quizservice/service/QuestionClient.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(url = "http://localhost:8080/", value = "Question-Client")
+//@FeignClient(url = "http://localhost:8080/", value = "Question-Client")
+@FeignClient(name = "QUESTION-SERVER")
 public interface QuestionClient {
 
     @GetMapping("/question/quiz/{quizId}")
